642447e2a552c963e9dc4f1a3fbcf54f2677fb6a
King Of Nothing <143756812+Osoapy@users.noreply.github.com>
2023-09-17 14:21:33 -0300

    Adicionando o contador e o "escutador" da página
    O contador serve pra manter uma base de quantos jogos ainda existem e o escutador vai saber quando a página for recarregada para ler todos os dados armazenados no localstorage. Além disso fiz os objetos armazenados quando forem lidos também serem possíveis de estar nos favoritos

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// INICIALIZANDO O CONTADOR DE JOGOS
+contador = localStorage.getItem("contador")
+if (contador == null) {
+  // CASO SEJA A PRIMEIRA VEZ ADICIONANDO UM JOGO, O CONTADOR É 1
+  localStorage.setItem("contador", "1");
+  contador = 1;
+}
+
+if (contador == 1) { // SE NÃO TIVER NENHUM JOGO (CONTADOR == 1), DIGA QUE NÃO TEM JOGOS
+  // LOCALIZANDO A DIV
+  let listaDeJogosDiv = document.getElementById("lista_de_normais");
+  // CRIANDO O SPAN E O TEXTO
+  let spanElement = document.createElement("span");
+  spanElement.id = "sem_jogos";
+  let h2Element1 = document.createElement("h1");
+  // COLOCANDO O SPAN NA LISTA
+  listaDeJogosDiv.appendChild(spanElement);
+  // COLOCANDO O TEXTO DENTRO DO SPAN
+  h2Element1.textContent = "Nenhum jogo foi adicionado ainda :(";
+  spanElement.appendChild(h2Element1);
+}
+
+
 // CRIANDO A FUNÇÃO ADICIONAR JOGO
 // A FUNÇÃO ADICIONAR JOGO CRIA AS TAGS EM HTML PRA CADA JOGO, SEJA ELE UM JOGO COLOCADO NA SESSÃO ATUAL OU ARMAZENADO NO LOCALSTORAGE
 function adicionarJogo(nome, descricao, favorito, novoJogo) {
@@ -32,7 +55,7 @@ function adicionarJogo(nome, descricao, favorito, novoJogo) {
     }
       
     else { // SE NÃO FOR FAVORITO, COLOQUE NA LISTA DE JOGOS NORMAIS
-      let listaDeJogosDiv = document.getElementById("lista_de_jogos");
+      let listaDeJogosDiv = document.getElementById("lista_de_normais");
       listaDeJogosDiv.appendChild(spanElement);
     }
   }
@@ -46,8 +69,15 @@ function adicionarJogo(nome, descricao, favorito, novoJogo) {
     spanElement.appendChild(h1Element);
     spanElement.appendChild(h2Element);
     // ENCONTRANDO A DIV E INCLUINDO O SPAN NELA
-    var listaDeJogosDiv = document.getElementById("lista_de_jogos");
-    listaDeJogosDiv.appendChild(spanElement);
+    if (favorito == 1) { // SE FOR FAVORITO, COLOQUE NA LISTA DE FAVORITOS
+      let listaDeFavoritosDiv = document.getElementById("lista_de_favoritos");
+      listaDeFavoritosDiv.appendChild(spanElement);
+    }
+      
+    else { // SE NÃO FOR FAVORITO, COLOQUE NA LISTA DE JOGOS NORMAIS
+      let listaDeJogosDiv = document.getElementById("lista_de_normais");
+      listaDeJogosDiv.appendChild(spanElement);
+    }
   }
 }
 
@@ -67,6 +97,7 @@ function adicionar(event) {
       break; // SAIR DO LOOP CASO TENHA ENCONTRADO UM BOTÃO MARCADO
     }
   }
+  
   if (botao_checado == 0) { // SE O BOTÃO CHECADO FOR O PRIMEIRO (o Sim) ADICIONE COMO FAVORITO
     if(nome !== "" && descricao !== "") { // CONFERINDO SE OS CAMPOS NOME E DESCRIÇÃO ESTÃO PREENCHIDOS
       adicionarJogo(nome, descricao, 1, 1);
@@ -75,6 +106,7 @@ function adicionar(event) {
       document.getElementById("descricao_do_jogo").value = ""; 
     }
   }
+    
   else if (botao_checado == 1) { // SENÃO SE O BOTÃO CHECADO FOR O SEGUNDO (o Não) ADICIONE COMO NÃO-FAVORITO
     if(nome !== "" && descricao !== "") { // CONFERINDO SE OS CAMPOS NOME E DESCRIÇÃO ESTÃO PREENCHIDOS
       adicionarJogo(nome, descricao, 0, 1);
@@ -85,3 +117,32 @@ function adicionar(event) {
   }
   // POR LÓGICA, SE O USUÁRIO NÃO MARCOU "SIM" OU "NÃO" PRA FAVORITO, POR PADRÃO NÃO IREMOS ADICIONAR O JOGO
 }
+
+
+
+
+
+
+
+
+
+// CRIANDO A FUNÇÃO INICIAR APÓS RECARGA
+function iniciarAposRecarga() { // UM PROBLEMA É DESCOBRIR QUANDO A PÁGINA FOI REINICIADA PARA OLHAR O LOCALSTORAGE
+  // LENDO TODOS OS OBJETOS ARMAZENADOS E ADICIONANDO CADA UM DELES A LISTA (ORDEM DE ADIÇÃO)
+  for(let k = 1; k < contador; k++){
+    let objetoSerializado = localStorage.getItem(`objeto${k}`);
+    if (objetoSerializado){
+      let Objeto = JSON.parse(objetoSerializado); // TRANSFORMANDO UMA STRING EM OBJETO DE NOVO
+      adicionarJogo(Objeto["nome"], Objeto["descricao"], Objeto["favorito"], 0);
+    }
+  }
+}
+window.addEventListener("beforeunload", function() { // TODA VEZ QUE FOR DAR F5 COLOCAR UM ITEM "PÁGINA RECARREGADA" COM VALOR TRUE
+  localStorage.setItem("paginaRecarregada", "true");
+});
+window.addEventListener("load", function() { // TODA VEZ QUE CARREGAR FAZER A LEITURA DOS DADOS
+  if (localStorage.getItem("paginaRecarregada") === "true") { // SE EXISTIR UM ITEM "PÁGINA RECARREGADA: TRUE" FAÇA O PROCESSO DE INICIAR APÓS RECARGA
+    iniciarAposRecarga();
+    localStorage.removeItem("paginaRecarregada");
+  }
+});
